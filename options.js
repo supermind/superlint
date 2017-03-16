470f90969eb73d8445af86adf5e2ad076eb6ca26
Matthew Wagerfield <matthew@wagerfield.com>
2017-03-16 18:27:49 +0000

    updated options to use inline config

--- options.js
+++ options.js
@@ -10,18 +10,19 @@ module.exports = {
   cmd: 'superlint',
   tagline: 'Supermind linting standards',
   eslintConfig: {
-    configFile: path.join(__dirname, 'eslintrc.js')
+    extends: [ 'supermind' ]
+    // configFile: path.join(__dirname, 'eslintrc.js')
   },
   parseOpts(options, config) {
+    console.log('--- options ---')
     console.log(options)
-    console.log('----------')
+    console.log('--- config ---')
     console.log(config)
-    console.log('----------')
-    if (config.use) {
-      var eslintrc = require('./eslintrc')
-      eslintrc.extends.concat(config.use)
-      console.log(eslintrc)
-    }
+    // if (config.use) {
+    //   var eslintrc = require('./eslintrc')
+    //   eslintrc.extends.concat(config.use)
+    //   console.log(eslintrc)
+    // }
     return options
   }
 }
